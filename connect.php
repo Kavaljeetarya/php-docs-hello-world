318f13916289b9c0dc15186e3d89d3b6d2e9243d
KJ <106296876+Kavaljeetarya@users.noreply.github.com>
2022-10-13 13:14:08 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,24 +1,24 @@
 <?php
-$host = 'appdbmysqlserver.mysql.database.azure.com';
-$username = 'fpbkyncxoo';
-$password = 'BS316RHI8702807V$';
+$host = 'njkmserver.mysql.database.azure.com';
+$username = 'njkmserveruser';
+$password = 'Arya@12345';
 $db_name = 'testdb';
 
 //Initializes MySQLi
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 // Establish the connection
-mysqli_real_connect($conn, 'appdbmysqlserver.mysql.database.azure.com', 'fpbkyncxoo', 'BS316RHI8702807V$', 'testdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'njkmserver.mysql.database.azure.com', 'njkmserveruser', 'Arya@12345', 'testdb', 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 //If connection failed, show the error
 if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-// //Run the Select query
-// printf("Reading data from table: \n");
-// $res = mysqli_query($conn, 'SELECT * FROM Products');
-// while ($row = mysqli_fetch_assoc($res))
-// {
-//     var_dump($row);
-// }
+//Run the Select query
+printf("Reading data from table: \n");
+$res = mysqli_query($conn, 'SELECT * FROM dinner');
+while ($row = mysqli_fetch_assoc($res))
+{
+   var_dump($row);
+}
 ?>
