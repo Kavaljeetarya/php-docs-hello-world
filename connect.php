dc9673ccbdcfc28aec4594cb609b0d1066029b6d
KJ <106296876+Kavaljeetarya@users.noreply.github.com>
2022-10-13 11:34:09 +0530

    new

--- connect.php
+++ connect.php
@@ -6,12 +6,9 @@ $db_name = testdb';
 
 //Initializes MySQLi
 $conn = mysqli_init();
-
 mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
 // Establish the connection
 mysqli_real_connect($conn, 'appdbmysqlserver.mysql.database.azure.com', 'fpbkyncxoo', 'BS316RHI8702807V$', 'testdb', 3306, NULL, MYSQLI_CLIENT_SSL);
-
 //If connection failed, show the error
 if (mysqli_connect_errno())
 {
@@ -21,7 +18,7 @@ if (mysqli_connect_errno())
 printf("Reading data from table: \n");
 $res = mysqli_query($conn, 'SELECT * FROM Products');
 while ($row = mysqli_fetch_assoc($res))
- {
+{
     var_dump($row);
- }
+}
 ?>
