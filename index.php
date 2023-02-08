90163af85726fb64a3cd21eb4ab46166570e7c0c
KJ <106296876+Kavaljeetarya@users.noreply.github.com>
2023-02-08 17:50:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,27 +3,28 @@
     
 $host = 'njkmserver.mysql.database.azure.com';
 $username = 'njkmserveruser';
-$password = 'Arya@12345';
+$password = 'Password@12345';
 $db_name = 'testdb';
 
 //Initializes MySQLi
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+#mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 // Establish the connection
-mysqli_real_connect($conn, 'njkmserver.mysql.database.azure.com', 'njkmserveruser', 'Arya@12345', 'testdb', 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+mysqli_real_connect($conn, "njkmserver.mysql.database.azure.com", "njkmserveruser", "Password@12345", "testdb", 3306, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 //If connection failed, show the error
-if (mysqli_connect_errno())
+if (mysqli_connect_error())
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_errno());
 }
-//Run the Select query
+echo "Run the Select query";
 printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM dinner');
+$res = mysqli_query($conn, 'SELECT * FROM dinners');
 while ($row = mysqli_fetch_assoc($res))
 {
-    echo $row['dinnerid']." ";
-    echo $row['dinnername']." ";
-    echo $row['dinnerday']." ";
-    echo $row['dinneraddress']."</br>";
+    echo $row['name']." ";
+    echo $row['birthsate']." ";
+    echo $row['entree']." ";
+    echo $row['side']." ";
+    echo $row['dessert']."</br>";
 }
 ?>
